ed0cad17e41b9d2868e3d868df922617538c2bdb
alejo <alejo@micorreo.com>
2024-10-03 18:13:30 -0400

    model added

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Publication(models.Model):
+    text = models.TextField()
+
+    def __str__(self) -> str:
+        return self.text[:50]
