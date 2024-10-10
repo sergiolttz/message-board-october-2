98309b5edd733251e880c48e90a831d0814a5441
alejo <alejo@micorreo.com>
2024-10-10 17:59:39 -0400

    post create new added

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostCreateView
 
 urlpatterns = [
     path("", PostListView.as_view(), name="post_list"),
+    path("post/new/", PostCreateView.as_view(), name="post_new"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from .models import Publication
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 class PostListView(ListView):
     model = Publication
     template_name = "post_list.html"
+
+class PostCreateView(CreateView):
+    model = Publication
+    template_name = "post_new.html"
+    fields =["text",] #todos los atributos a incluir en la vista
+    
